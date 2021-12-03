16437c48a2c6bafea68d8aab42575205bedb38c8
Walaa Faqeh <81427224+walaa2211@users.noreply.github.com>
2021-12-03 23:25:20 +0200

    Add files via upload

---
+++ pop.java
@@ -0,0 +1,28 @@
+
+ import java.io.* ;
+import java.net.*;
+ import java.util.*;
+ import java.util.concurrent.locks.* ;
+ public class pop
+ {
+ private final static int MAX_NO OF CONNECTION =4; public static void main (String args[]){
+try
+{
+	connection connection;
+	sharedConnection con;
+}
+ 
+
+public void run ()
+{
+	try
+	{
+		con clean ();
+		
+	}
+	catch(Exception e)
+	{
+		e.print protocol();
+	}
+}
+}
